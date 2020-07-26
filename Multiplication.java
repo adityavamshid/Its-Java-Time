23d6095a9bd3871f0049c9b2d4e77fd06dd16fca
adityavamshid <68259533+adityavamshid@users.noreply.github.com>
2020-07-27 04:45:57 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+package myclass;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 19, num2 = 2, mul;
+		mul = num1 * num2;
+		System.out.println("the multiplication is " +mul);
+
+	}
+
+}
---
+++ Substraction.java
@@ -0,0 +1,13 @@
+package myclass;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 20, num2 = 17, sub;
+		sub = num1 - num2;
+		System.out.println("the substraction of these  numbers is " +sub);
+
+	}
+
+}
