95f20a4604b596693ec46ac63c44bd7ad41ce64c
adityavamshid <68259533+adityavamshid@users.noreply.github.com>
2020-07-27 04:47:18 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package myclass;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub}
+		int num1 = 5, num2 = 20, sum;
+		sum = num1 + num2;
+		System.out.println("sum of these numbersis :"+sum);
+	
+		}
+	}
+
+
